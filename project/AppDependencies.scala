668aeedeab387a21a7e1d56748972e819809cdf7
RobertLaverick <10117445+RobertLaverick@users.noreply.github.com>
2026-02-25 16:21:25 +0000

    updated appdependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  lazy val bootStrapPlayVersion = "9.19.0"
+  lazy val bootStrapPlayVersion = "10.6.0"
 
   lazy val compile: Seq[ModuleID] = Seq(
     ws,
@@ -10,6 +10,6 @@ object AppDependencies {
   )
 
   lazy val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-30"     % bootStrapPlayVersion
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootStrapPlayVersion
   ).map(_ % Test)
 }
